a85a4f7988d677701b55f686482cf35b6b50b2af
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-21 13:59:16 -0800

    chapter 1 done

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const bodyParser = require('body-parser');
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', {msg: 'Handlebars are cool'})
 })
 
 app.listen(3000, () => {
